23436820ced57f0247e50348c572de2a46822e2c
Alex Maximilian <alexandervanpoecke1@gmail.com>
2021-11-01 19:11:26 +0000

    Made some improvments/edits

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
+#Alex Maximilian
+#Simple string reversal script using slicing
+
+
 def reverse_a_string(x):
   return x[::-1]
 
 x = input("Input your text you want to reverse")
 mytxt = reverse_a_string(x)
 
-print(mytxt)
+print("Your orignal text was", x, "backwards it is", mytxt)
+
